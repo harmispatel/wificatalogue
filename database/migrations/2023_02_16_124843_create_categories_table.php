8f3cf98030a6dcb4e9ac5c6abd55e2df68e70a01
harmistest <harmistest@gmail.com>
2023-02-17 13:07:54 +0530

    Category Module

--- database/migrations/2023_02_16_124843_create_categories_table.php
+++ database/migrations/2023_02_16_124843_create_categories_table.php
@@ -15,6 +15,7 @@ class CreateCategoriesTable extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->integer('shop_id')->nullable();
             $table->string('name')->nullable();
             $table->text('description')->nullable();
 
